d33259701e4cccc7a1b1a5a704160083bfb743fe
megait402 <megait402@gmail.com>
2024-09-30 19:14:29 +0900

    [전진우] - 인터셉터 기능 완성

--- src/main/java/com/jeonpeace/memo/config/WebMvcConfig.java
+++ src/main/java/com/jeonpeace/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.jeonpeace.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.jeonpeace.memo.common.FileManager;
+import com.jeonpeace.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
@@ -13,6 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	  registry.addResourceHandler("/images/**")
 	  .addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/static/**", "/images/**", "/user/logout");
 	}
 	
 }
